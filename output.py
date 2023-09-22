582001ff37043354ebc8648cb012b34f38c0d231
raaasin <nisarvskp@gmail.com>
2023-09-22 22:28:53 +0530

    pause

--- output.py
+++ output.py
@@ -1,4 +1,14 @@
 from qna import pipe
+from haystack.utils import print_answers
+import io
+import sys
+
+output_buffer = io.StringIO()
+sys.stdout = output_buffer
 prediction = pipe.run(
-    query="Who is the father of Arya Stark?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
+    query="what is mining?", params={"Retriever": {"top_k": 10}, "Reader": {"top_k": 5}}
 )
+print_answers(prediction, details="minimal")
+sys.stdout = sys.__stdout__
+a= output_buffer.getvalue()
+print(a)
